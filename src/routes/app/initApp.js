5c58d2ade1d20c0cdb5e12b925e09dafeaff161e
carlos-hereee <97hernandez.c@gmail.com>
2023-11-26 04:29:59 -0600

    add initial menu to app build

--- src/routes/app/initApp.js
+++ src/routes/app/initApp.js
@@ -1,5 +1,6 @@
 const useGenericErrors = require("../../utils/auth/useGenericErrors");
 const createApp = require("../../db/models/app/createApp");
+const formatInitMenu = require("../../utils/app/format/formatInitMenu");
 // const formatThemeList = require("../../utils/app/format/formatThemeList");
 // const formatLanguageList = require("../../utils/app/format/formatLanguageList");
 
@@ -8,7 +9,8 @@ module.exports = async (req, res, next) => {
     // key variables
     const appName = req.body.appName || req.parms.appName;
     const owner = req.user._id;
-    const app = await createApp({ appName, logo: req.logo, owner, adminIds: [owner] });
+    const menu = formatInitMenu();
+    const app = await createApp({ appName, logo: req.logo, owner, adminIds: [owner], menu });
     // add user permissions
     req.user.ownedApps.push(app._id);
     req.user.permissions.push({ appId: app._id, role: "owner" });
