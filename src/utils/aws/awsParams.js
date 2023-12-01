d322f75e59d14048431e6684a36b2c2c0ede6eaf
carlos-hereee <97hernandez.c@gmail.com>
2023-11-30 23:58:34 -0600

    add content type

--- src/utils/aws/awsParams.js
+++ src/utils/aws/awsParams.js
@@ -1,7 +1,12 @@
 const { awsBucketName } = require("../../../config.env");
 
 const generateParamFile = (file) => {
-  return { Bucket: awsBucketName, Key: Date.now() + "-" + file.originalname, Body: file.buffer };
+  return {
+    Bucket: awsBucketName,
+    Key: Date.now() + "-" + file.originalname,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+  };
 };
 const generateParamFiles = (files) => {
   return files.map((file) => generateParamFile(file));
