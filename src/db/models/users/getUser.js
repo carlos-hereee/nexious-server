d66807b1deda939b56ca51e511fe44de7bcc8f59
carlos-hereee <97hernandez.c@gmail.com>
2023-12-08 18:30:48 -0600

    add logo as part of selection

--- src/db/models/users/getUser.js
+++ src/db/models/users/getUser.js
@@ -8,7 +8,7 @@ module.exports = async ({ username, email, userId, all, appId }) => {
     // send data required by client
     return await Users.findOne({ userId }).populate({
       path: "ownedApps subscriptions",
-      select: "appName appId ownerId menu media owner",
+      select: "appName appId ownerId menu media owner logo",
       populate: { path: "owner", select: "userId url small alt heroId uid" },
     });
   }
