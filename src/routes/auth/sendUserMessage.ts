bdf9b45cd66ef8f0b95c8cbdb0801c91d96e6ef3
carlos-hereee <97hernandez.c@gmail.com>
2024-08-28 01:43:15 -0500

    DRY: get user name

--- src/routes/auth/sendUserMessage.ts
+++ src/routes/auth/sendUserMessage.ts
@@ -1,6 +1,7 @@
 import { AuthRequest } from "@app/request";
 import { getUser } from "@db/models/users/getUser";
 import Messages from "@db/schema/messages";
+import { generateUsername } from "@utils/app/generateStr";
 import { useGenericErrors } from "@utils/auth/useGenericErrors";
 import { NextFunction, Response } from "express";
 
@@ -9,17 +10,9 @@ export const sendUserMessage = async (req: AuthRequest, res: Response, next: Nex
     const recipientUser = await getUser({ userId: req.params.userId });
 
     if (!recipientUser) return res.status(400).json("unable to fiend recipient").end();
-    const user = {
-      userId: req.user.userId,
-      avatar: req.user.avatar,
-      name: req.user.nickname || req.user.name || req.user.username || req.user.email,
-    };
+    const user = { userId: req.user.userId, avatar: req.user.avatar, name: generateUsername(req.user) };
 
-    const recipient = {
-      userId: recipientUser.userId,
-      avatar: recipientUser.avatar,
-      name: recipientUser.nickname || recipientUser.name || recipientUser.username || recipientUser.email,
-    };
+    const recipient = { userId: recipientUser.userId, avatar: recipientUser.avatar, name: generateUsername(recipientUser) };
     // // create message
     const message = await Messages.create({ ...req.body, user, recipientRole: "friend", recipient });
     // // link message
--- src/utils/app/generateStr.ts
+++ src/utils/app/generateStr.ts
@@ -3,7 +3,7 @@ import { today12hr } from "./format/generateDate";
 import { clientUrl } from "./config";
 
 export const toLowerCase = (str: string) => str.toLowerCase();
-export const userName = (user: IUserSchema) => user.name || user.nickname || user.username || user.email || "";
+export const generateUsername = (user: IUserSchema) => user.nickname || user.name || user.username || user.email || "";
 export const generatePasswordResetUrl = (id: string) => clientUrl + "/reset-password/?userid=" + id;
 // TODO:  "Location of Change: {{City, Country (if applicable)}}",
 // TODO:   "- Review your account activity: {{Account Activity Link}}",,
@@ -13,7 +13,7 @@ export const generateMadLib = (textData: string[], madLib: string[], user: IUser
       const targetIdx = madLib.findIndex((val) => str.includes(val));
       if (targetIdx >= 0) {
         const command = madLib[targetIdx];
-        if (command === "{{name}}") return str.replace(command, userName(user));
+        if (command === "{{name}}") return str.replace(command, generateUsername(user));
         if (command === "{{date-and-time}}") return str.replace(command, today12hr());
         if (command === "{{resetPasswordLink}}") return str.replace(command, generatePasswordResetUrl(user.userId));
         if (command === "{{support-email}}") return str.replace(command, "carlos.h@nexious.tech");
