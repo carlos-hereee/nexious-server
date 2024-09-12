df240db27d5009d1b3248390b66dc8c6e0b8e22d
carlos-hereee <97hernandez.c@gmail.com>
2024-09-12 04:38:24 -0500

    toggle member reassignment

--- src/routes/app/tasks/assignMemberToTask.ts
+++ src/routes/app/tasks/assignMemberToTask.ts
@@ -3,18 +3,23 @@ import { getTaskWithId } from "@db/models/app/getTaskBoard";
 import { useGenericErrors } from "@utils/auth/useGenericErrors";
 import { NextFunction, Response } from "express";
 
-export const assignMemberToTask = async (req: AppRequest, res: Response, next: NextFunction) => {
+export const assignMemberToTask = async (req: AppRequest<{ status: string }>, res: Response, next: NextFunction) => {
   try {
-    // find member
-    const member = req.taskBoard.members.filter((m) => m.userId === req.params.userId);
     // find task
     const task = await getTaskWithId({ taskId: req.params.taskId });
+    if (!task) return res.status(404).end();
     // if both are found
-    if (member[0] && task) {
-      // add to task
-      task.assignedTo?.push(member[0]);
-      await task.save();
+    if (req.body.status === "remove") {
+      task.assignedTo = task?.assignedTo?.filter((m) => m.userId !== req.params.userId);
     }
+    if (req.body.status === "assign") {
+      // find member
+      const member = req.taskBoard.members.filter((m) => m.userId === req.params.userId);
+      // add to task
+      if (member[0]) task.assignedTo?.push(member[0]);
+    }
+    await task.save();
+    await req.taskBoard.save();
     next();
   } catch (error) {
     useGenericErrors(res, error, "error registering user");
